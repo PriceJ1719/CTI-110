7a88074c79592cca105775580b2e6690150a2b79
PriceJ1719 <pricej1719@student.faytechcc.edu>
2025-09-18 08:25:14 -0400

    Add files via upload

---
+++ P1LAB1_pricejason.py
@@ -0,0 +1,11 @@
+# jason price
+# 18 SEP 25
+# P1LAB1
+# this program asks for first and last name and then displays a message
+
+# prompt user for first and last names
+first_name = input("please enter your first name: ")
+last_name = input("please enter your last name: ")
+
+# display welcome message
+print("hello,", first_name, last_name + "! welcome to CTI-110")
